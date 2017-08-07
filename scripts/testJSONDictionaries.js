0c4f34390bb93c7d695436ef45e1e0a4283a4455
Thomas Arrow <thomasarrow@gmail.com>
2017-08-07 10:03:30 +0100

    script to return names of unnamed dictionaries

---
+++ scripts/testJSONDictionaries.js
@@ -0,0 +1,32 @@
+var fs = require('fs')
+var util = require('util')
+var _ = require('lodash')
+
+var fsRDP = util.promisify(fs.readdir)
+var fsRFP = util.promisify(fs.readFile)
+
+function readJsonFiles (filenames) {
+  var foo = filenames.map((file) => {
+    return fsRFP(file, { encoding: 'utf8' })
+  })
+  return (Promise.all(foo))
+}
+
+process.chdir('./json')
+fsRDP('./')
+.then(readJsonFiles)
+.then(function (files) {
+  files.map(function (file) {
+    var dict = JSON.parse(file)
+    if (dict.id) {
+      // console.log(dict.id)
+    } else {
+      var possibleTitles = dict.entries.map(function (entry) {
+        return entry.identifiers.contentmine.match(/^([a-zA-Z.]*)/)[1]
+      })
+      // console.log(possibleTitles)
+      console.log(_.uniq(possibleTitles))
+    }
+  })
+})
+.catch(console.log)
